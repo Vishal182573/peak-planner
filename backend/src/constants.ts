7c6039c7fd1b0686597a398b1cc86db39a7791f0
vishal sharma <sharmavs9205@gmail.com>
2025-03-01 15:24:38 +0530

    cors resolve

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,4 +1,5 @@
 // const FRONTEND_URL = "http://localhost:5173";
-const FRONTEND_URL = "https://peak-planner.vercel.app";
+// const FRONTEND_URL = "https://peak-planner.vercel.app";
+const FRONTEND_URL = "https://peak-planner-n4qp.vercel.app";   // latest
 
 export { FRONTEND_URL };
--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "http://localhost:5000"
-export const BACKEND_URL = "https://peak-planner.onrender.com"
+// export const BACKEND_URL = "https://peak-planner.onrender.com"
+export const BACKEND_URL = "https://peak-planner-9lu8.onrender.com"  // latest
